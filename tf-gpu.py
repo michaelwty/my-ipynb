061c4438be7e2fe22bc9e93a3b100d4fff00f24c
Tengyan Wang <wty1009@outlook.com>
2019-10-26 10:00:29 +0800

    tf 2.0

--- tf-gpu.py
+++ tf-gpu.py
@@ -1,23 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import tensorflow as tf
-import os
 #查看版本号
 tf.__version__
 #查看安装路径
 tf.__path__
 
+
+# In[12]:
+
+
 #Creates a graph.
-a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-c = tf.matmul(a, b)
+g = tf.Graph()
+with g.as_default():
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+    
 #Creates a session with log_device_placement set to True.
-gpu_no = '0' # or '1'
-os.environ["CUDA_VISIBLE_DEVICES"] = gpu_no
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 config.log_device_placement = True
-sess = tf.Session(config=config)
+sess = tf.compat.v1.Session(config=config, graph=g)
 
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 #Runs the op.
 print (sess.run(c))
+sess.close()
+
+
+# In[ ]:
+
+
+
+
